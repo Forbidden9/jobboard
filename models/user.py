949a832f7400a5e9d322d20c148dc7c749c2d761
Forbidden <lloduardo@gmail.com>
2023-02-09 18:39:28 -0500

    Add() - Add new fields in table user.

--- models/user.py
+++ models/user.py
@@ -1,11 +1,15 @@
 from sqlalchemy import Table, Column
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean
 from db.session import engine, meta_data
 
 users = Table("users", meta_data,
-              Column("id", Integer, primary_key=True),
+              Column("id", Integer, primary_key=True, index=True),
               Column("name", String(255), nullable=False),
               Column("username", String(255), nullable=False),
-              Column("password", String(255), nullable=False, unique=True)
+              Column("email", String(255), nullable=False, unique=True, index=True),
+              Column("password", String(255), nullable=False, unique=True),
+              Column("is_active", Boolean, default=True),
+              Column("is_superuser", Boolean, default=False)
               )
+
 meta_data.create_all(engine)
