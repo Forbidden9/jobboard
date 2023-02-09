4fa27128b6a8359e44989477c7644dcafcf18711
Forbidden <lloduardo@gmail.com>
2023-02-09 18:29:47 -0500

    Edit() - Edit session conection and delete sqlite conection.

--- db/session.py
+++ db/session.py
@@ -6,15 +6,16 @@ from config.config import settings
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# if you don't want to install postgres or any database, use sqlite, a file system based database,
-# uncomment below lines if you would like to use sqlite and comment above 2 lines of SQLALCHEMY_DATABASE_URL AND engine
-
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-
 meta_data = MetaData()
 
 # Transaccion para la serie de transacciones
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Conexion con una session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
