fb0e7be3a1767d5033d668475957f66a9950f014
Forbidden <lloduardo@gmail.com>
2023-02-09 18:33:12 -0500

    Edit() - Refactoritation main file.

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from config.config import settings
-from db.session import engine
-from db.base_class import Base
 from routers.user import user
 import uvicorn
 
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
-    app.include_router(user)
+    app.include_router(user, tags=["user"], prefix='/api/user')
     return app
 
-
-# app = start_application()
 
 if __name__ == "__main__":
     uvicorn.run(start_application(), host="127.0.0.1", port=8000, workers=True)
