0c4297f4e0dbcbd5e7940c508c73e4f81b697bc8
Forbidden <lloduardo@gmail.com>
2023-02-28 22:06:45 -0500

    Edit() - Edit car schema.

--- schemas/car/car.py
+++ schemas/car/car.py
@@ -19,22 +19,30 @@ class CarBaseSchema(BaseModel):
 
 
 class CreateCarSchema(CarBaseSchema):
-    pass
+    user_id: uuid.UUID
+
+
+class UpdateCarSchema(BaseModel):
+    car_license: Optional[str]
+    car_model: Optional[str]
+    car_color: Optional[str]
+    car_engine: Optional[str]
+    pick_up_at_airports: Optional[bool]
+    airport_delivery: Optional[bool]
+    short_distance_travel: Optional[bool]
+    long_distance_travel: Optional[bool]
+    user_id: Optional[uuid.UUID]
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+
+    class Config:
+        orm_mode = True
 
 
 class CarResponse(CarBaseSchema):
     id: uuid.UUID
     created_at: datetime
     updated_at: datetime
-
-
-class UpdateCarSchema(BaseModel):  # no hereda de CarBaseSchema
-    pass
-    # created_at: Optional[datetime, None] = None
-    # updated_at: Optional[datetime, None] = None
-
-    # class Config:
-    #     orm_mode = True
 
 
 class ListCarResponse(BaseModel):
